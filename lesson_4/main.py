b2085eb8f14f306c10ce2a43bfe79e80c079f769
Сергали <bakirovdvm@gmail.com>
2024-06-18 18:27:35 +0600

    4th lesson is done

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -31,3 +31,25 @@ my_file_1_copy['random_summ_from_numbs'] = my_file_1_copy['random_summ_from_numb
 print(f'\nОкругляем предпоследнюю колонку:\n{my_file_1_copy[:4]}')
 
 print(f'\nВспоминаем количество столбцов и строк: {my_file_1_copy.shape}')
+
+np.random.seed(123456)
+s_random = pd.Series(np.random.normal(size=891),
+                     index=my_file_1_copy.index)
+print(f'\nВыводим рандомные числа: \n{s_random[:4]}')
+my_file_1_copy.loc[:, 'random_numbs'] = s_random
+print(f'\nВыводим нашу табличку + новый столбец с рандомными числами: \n{my_file_1_copy[:5]}')
+
+print(f'\nВыводим столбцы нашей таблички в обратном порядке: \n{my_file_1_copy[my_file_1_copy.columns[::-1]][:5]}')
+
+my_file_1_copy['item_for_delete'] = s_random
+print(f'\nСоздали новый столбец для удаления: \n{my_file_1_copy[:3]}')
+del my_file_1_copy['item_for_delete']
+print(f'\nУдалили новый столбец для удаления при помощи комады del: \n{my_file_1_copy[:3]}\n')
+
+
+popped = my_file_1_copy.pop('random_summ_from_first_numbs')
+print(f'\nВыводим удаленный столбец при помощи команды pop:\n{popped}')
+print(f'\nВыводим таблицу без удаленного столбка: \n{my_file_1_copy[:5]}')
+
+dropped = my_file_1_copy.drop(['random_summ_from_numbs'], axis=1)
+print(f'\nВыводим данные из переменной dropped, в которой удалили столбец random_summ_from_numbs\n{dropped}')
